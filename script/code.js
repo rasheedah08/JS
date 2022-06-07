35c4546ea14ed0ef65cb0c9d20e2d81d766dd753
rasheedah08 <rasheedahanhuizen@gmail.com>
2022-06-07 11:22:37 +0200

    changes

---
+++ script/code.js
@@ -0,0 +1,70 @@
+// const btnDisplay = document.getElementById("btnDisplay");
+
+// btnDisplay.addEventListener('click', ()=>{
+//     document.getElementById('content').innerText = 'Rasheedah Anhuizen'
+// })
+
+// var myStr = "I am a \" double quoted\" string inside another \"double quoted\".";
+// console.log(myStr);
+
+// let firstName = 'Peter';
+// let lastName = 'henk';
+// let age = '17';
+// // console.log("My name is"
+// // +firstName+", \nSurname is "+lastName+",\nage is "+age+"years old");
+
+// console.log("My Name is " .concat(firstName, ", my Surname is ",lastName, ",Age is ",age, "years old"));
+
+// var hours = 30;
+// var rate = 12;
+// function show(){
+//     document.getElementById('delete').innerHTML = ''
+// }
+
+// function  MyFunction() {
+//     var hours = document.getElementById("hours").value;
+//     var rate = document.getElementById("rate").value;
+//     var salary = hours * rate;
+//     document.getElementById("here").innerHTML = "Your salary is " + "R"+ salary  ;
+// }
+// function myFunction1() {
+//   document.getElementById("myForm").reset();
+//   document.getElementById("here").innerHTML = "";
+// }
+
+// let age = 18;
+// let salary = 5000;
+// let bonus = 500;
+
+// if (age >= 18) {
+//   if(salary >= 5000) {
+//     salary += bonus;
+//     console.log('Salary R: '.concat(salary))
+//     ;
+//   }else {
+//     console.log('Your Salary is less than R5000');
+//   }
+// }
+
+// Collection
+// let BestFriends = ['Shannon Lee Jacobs' , 'Jean Jen Jacobs' , 'Anne Hathaway', 'Mia Gunner' , 'Adam Smith' , ' Jen Abrahams' , ' Kaylin Lewis ' , 'Will Smith' , 'Stuart Bloom '];
+// let ul = document.getElementById('BestFriends');
+// // display the name
+// b1 = BestFriends[5];
+//  console.log(b1);
+//  //displays the characters of the name
+// b2 = BestFriends[5].length;
+//  console.log(b2);
+// BestFriends.forEach((e) =>{
+//     ul.innerHTML = `
+//     <li> ${b1} </li>
+//     <li> ${b2} </li>
+//     `
+//     ;
+// });
+
+let bestFriends = ['Zara','Mia','Amy'];
+personInMiddle = bestFriends[1];
+console.log(personInMiddle);
+lastPerson = bestFriends[2];
+console.log(lastPerson);
